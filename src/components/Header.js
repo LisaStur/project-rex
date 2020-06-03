ebbaa2c66c87fc05a047555795913f612e262ff0
Lisas Sturegard <lisasturegard@hotmail.com>
2020-06-03 16:53:29 +0200

    Started with nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import { Link }from 'react-router-dom'
+import styled from 'styled-components'
 
 export const Header = () => {
+
   return (
-    <ul>
-      <li><Link to={'/'}>REX Animation</Link></li>
-      <li><Link to={'/movies'}>Movies</Link></li>
-      <li><Link to={'/programme'}>Programme</Link></li>
-      <li><Link to={'/venues'}>Venues</Link></li>
-      <li><Link to={'/skolbio'}>Skolbio</Link></li>
-      <li><Link to={'/awards'}>Awards</Link></li>
-      <li><Link to={'/aboutpage'}>About Rex</Link></li>
-    </ul>
+    <div>
+      <Navbar>
+        <StyledLink to={'/'}>REX Animation</StyledLink>
+        <StyledLink to={'/movies'}>Movies</StyledLink>
+        <StyledLink to={'/programme'}>Programme</StyledLink>
+        <StyledLink to={'/venues'}>Venues</StyledLink>
+        <StyledLink to={'/skolbio'}>Skolbio</StyledLink>
+        <StyledLink to={'/awards'}>Awards</StyledLink>
+        <StyledLink to={'/aboutpage'}>About Rex</StyledLink>
+      </Navbar>
+    </div>
   )
 }
+
+const Navbar = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  
+  @media (min-width: 668px) {
+    flex-flow: row wrap;
+  }
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  padding-right: 16px;
+
+  &:hover {
+    color: red;
+  }
+`
