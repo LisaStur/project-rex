3ebea00b3d18bce6e4f7fe3e3a9225fac77c5361
Lisas Sturegard <lisasturegard@hotmail.com>
2020-06-08 17:12:35 +0200

    Connecting frontend with backend

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+
 
 export const Movies = () => {
+  const URL_MOVIES = 'http://localhost:8080/movies'
+ const [movies, setMovies]= useState([])
+
+  useEffect(() => {
+    fetch(URL_MOVIES)
+      .then((res) => {
+        return res.json()
+      })
+      .then(data => {
+        setMovies(data)
+      })
+  }, [])
 
   return (
-    <h1>Movies</h1>
+    <div>   
+      {movies.map(movie => (
+        <section key={movie._id}>
+          <h1> {movie.title}</h1>
+          <h3>{movie.originalTitle}</h3>
+          <h2>{movie.director}</h2>
+          <h3>{movie.country} {movie.productionYear} {movie.duration}min {movie.language}</h3>
+          <p>{movie.synopsis}</p>
+        </section>
+      ))}
+    </div>
   )
 }
